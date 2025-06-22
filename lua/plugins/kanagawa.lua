c5cd2601df81e9c9bebc19b0dfcd28e7923a4b0f
RyzenPC <luc@Ryzen9.localdomain>
2025-06-22 03:26:26 -0400

    first commit

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,21 @@
+return { 
+	"rebelot/kanagawa.nvim", 
+	config = function()
+		require('kanagawa').setup({
+			compile = true,
+			transparent = true,
+			theme = 'wave',
+			    background = {               -- map the value of 'background' option to a theme
+        dark = "dragon",           -- try "dragon" !
+        light = "lotus"
+    },
+		});
+
+		vim.cmd("colorscheme kanagawa");
+		
+	end,
+
+	build = function()
+		vim.cmd("KanagawaCompile");
+	end
+}	
